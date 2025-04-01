f97f24a2fdae7abae520ca7fe0d1434ac474e4f0
alk51 <Aleksandar.Kratinov@movedigital.ch>
2025-04-01 23:08:22 +0100

    - Remove variables

--- test/test_vector.py
+++ test/test_vector.py
@@ -1,10 +1,6 @@
 import pytest
 
-from Vector import Vector
 from mock.mock_vector import expected_pairs
-
-a = Vector(3, 5)
-b = Vector(2, 4)
 
 
 @pytest.mark.parametrize("vector, expected_value", expected_pairs)
