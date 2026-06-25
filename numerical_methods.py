cf6bd3c403c3e97a22c0fdc8d17d6da8abc6a374
BigChunguz24 <alk511@outlook.com>
2026-06-25 03:48:37 +0300

    - Euler-Cromer method with animation

--- numerical_methods.py
+++ numerical_methods.py
@@ -37,3 +37,29 @@ def perform_euler(
         vy += ay * delta_t
 
     return pd.DataFrame(rows, columns=column_labels)
+
+
+def perform_euler_cromer(
+    initial_x: float,
+    initial_y: float,
+    initial_vx: float,
+    initial_vy: float,
+    acc_function: Callable[[float, float], tuple[float, float]],
+    iterations: int,
+    delta_t: float,
+) -> pd.DataFrame:
+    x, y = initial_x, initial_y
+    vx, vy = initial_vx, initial_vy
+
+    rows = []
+    for i in range(iterations):
+        ax, ay = acc_function(x, y)
+        rows.append((i * delta_t, x, y, vx, vy, ax, ay))
+
+        vx += ax * delta_t
+        vy += ay * delta_t
+
+        x += vx * delta_t
+        y += vy * delta_t
+
+    return pd.DataFrame(rows, columns=column_labels)
