2c066b1bcd5d0e8539d2972a4c9ff368a1a84615
BigChunguz24 <alk511@outlook.com>
2026-09-24 01:47:43 +0300

    - Uploading Files

--- Website.py
+++ Website.py
@@ -2,10 +2,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+problem_sets_junior = [
+    {
+        "title": "Junior Physics Olympiad 17",
+        "file": "junior_17.pdf"
+    },
+]
+
+problem_sets_senior = [
+{
+        "title": "Senior Physics Olympiad 21",
+        "file": "senior_21.pdf"
+    },
+]
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template(
+        template_name_or_list="index.html",
+        problem_sets_junior=problem_sets_junior,
+        problem_sets_senior=problem_sets_senior,
+    )
 
 
 if __name__ == "__main__":
