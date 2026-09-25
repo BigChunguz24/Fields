60653a67f951304b43f66224947b13b58afe8c64
BigChunguz24 <alk511@outlook.com>
2026-09-25 18:08:47 +0300

    - Hardcode Constants // PythonAnywhere has memory limits and scipy takes too much of it

--- physical_constants.py
+++ physical_constants.py
@@ -1,13 +1,13 @@
-from scipy import constants
+pi = 3.141592653589793
 
 # Used to check zero norms
 eps = 1e-6
 
 # Uniform gravity
-uniform_g = constants.g
+uniform_g = 9.80665
 
 # Dielectric permittivity
-varepsilon_0 = constants.epsilon_0
+varepsilon_0 = 8.8541878188e-12
 
 # Coulomb constant
-k = 1 / 4 / constants.pi / varepsilon_0
+k = 1 / (4 * pi * varepsilon_0)
